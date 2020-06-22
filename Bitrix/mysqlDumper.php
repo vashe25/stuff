35640cf0fcfe3b32cb312c02dacdd52cdfb8366f
vshevchenko <vshevchenko@dalee.ru>
2020-06-22 15:18:50 +0700

    dump single-transaction quick

--- Bitrix/mysqlDumper.php
+++ Bitrix/mysqlDumper.php
@@ -1,5 +1,5 @@
 <?php
-exec("mysqldump -u{$DBLogin} -p{$DBPassword} -h{$DBHost} --databases {$DBName} | gzip > {$_SERVER['DOCUMENT_ROOT']}/upload/{$DBName}.sql.gz");
+exec("mysqldump -u{$DBLogin} -p{$DBPassword} -h{$DBHost} --single-transaction --quick --databases {$DBName}  | gzip > {$_SERVER['DOCUMENT_ROOT']}/upload/{$DBName}.sql.gz");
 echo "{$_SERVER['HTTP_HOST']}/upload/{$DBName}.sql.gz";
 
 // unlink("{$_SERVER['DOCUMENT_ROOT']}/upload/{$DBName}.sql.gz");
